25d390ea0f4cc06b690635ba28bb61958d091f39
Shehroz Ali <sshehroza13@gmail.com>
2022-01-17 00:55:32 +0500

    Test Unit Successfully passed

--- src/test/java/com/example/demo/AdminControllerTest.java
+++ src/test/java/com/example/demo/AdminControllerTest.java
@@ -2,10 +2,26 @@ package com.example.demo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 public class AdminControllerTest {
@@ -35,5 +51,22 @@ public class AdminControllerTest {
 
     Feedback EMPLOYEE_FEEDBACK_1 = new Feedback(1, "I Know Hasan and he is excellent perogramer");
 
-    
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(adminController).build();
+    }
+
+    @Test
+    public void getallEmployeesData_success() throws Exception {
+        List<Data> employeedata = new ArrayList<>();
+        employeedata.add(EMPLOYEE_DATA_1);
+        employeedata.add(EMPLOYEE_DATA_2);
+
+        Mockito.when(databaseRepository.findAll()).thenReturn(employeedata);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/employee/all")
+                .contentType(MediaType.APPLICATION_JSON));
+    }
 }
